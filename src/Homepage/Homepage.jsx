6f064b137c7d008f462dab4d6d0adc540b4ecb5a
kidist-hayalu <hkidist18hayalu@gmail.com>
2025-09-24 15:10:14 +0300

    changes

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -22,10 +22,10 @@ function Homepage() {
                         <Link to={'/Login'} className='text-white '>Start project</Link>
                     </div>
                 </div>
-                <div className='flex flex-row pr-20  my-4'>
+                <div className='flex flex-row pr-20  my-44'>
 
 
-                    <div className='p-0 h-full flex flex-col items-center justify-center text-black my-24 rounded-md mr-80 w-4/6'>
+                    <div className='p-0 h-full flex flex-col items-center justify-center text-black  rounded-md mr-80 w-4/6'>
                         <Stacks />
                     </div>
 
@@ -35,7 +35,7 @@ function Homepage() {
 
 
 
-                    <motion.div className='p-10 ml-36 flex flex-col items-center justify-self-end border-2 border-blue-500 text-black h-full my-24 rounded-md w-2/6'
+                    <motion.div className='p-10 mx-6 flex flex-col items-center justify-self-end border-2 border-blue-500 text-black h-full rounded-md w-2/6'
                         whileInView={{ scale: [1, 1, 1, 1], rotate: [0, -15, 0, 0] }}
                         viewport={{ once: true, amount: 0.5 }}
                         transition={{ duration: 5, ease: "easeInOut", repeat: Infinity, repeatDelay: 1 }}>
@@ -47,10 +47,10 @@ function Homepage() {
                 <div className='flex flex-row h-screen px-10'>
                     <div className='p-10 ml-10 flex flex-col items-center justify-center w-4/5'>
                         <div className="relative flex items-center justify-center w-full h-full">
-                            <div className="absolute inset-0 z-10 bg-red-500 w-full h-full origin-left transition transform duration-500 hover:scale-x-[1.2]"></div>
-                            <div className="absolute inset-0 z-[15] bg-green-500 w-4/6 h-full origin-left transition transform duration-500 hover:scale-x-[1.2]"></div>
-                            <div className="absolute inset-0 z-20 bg-cyan-500 w-2/6 h-full origin-left transition transform duration-500 hover:scale-x-[1.25]"></div>
-                            <div className="absolute z-[25] inset-0  p-4 bg-blue-500 rounded w-1/6 h-full origin-left transform transition duration-500 hover:scale-x-[1.2] overflow-hidden"></div>
+                            <div className="absolute inset-0 z-10 bg-blue-600 w-full h-full origin-left transition transform duration-500 hover:scale-x-[1.2]"></div>
+                            <div className="absolute inset-0 z-[15] bg-blue-700 w-4/6 h-full origin-left transition transform duration-500 hover:scale-x-[1.2]"></div>
+                            <div className="absolute inset-0 z-20 bg-blue-800 w-2/6 h-full origin-left transition transform duration-500 hover:scale-x-[1.25]"></div>
+                            <div className="absolute z-[25] inset-0  p-4 bg-blue-900 rounded w-1/6 h-full origin-left transform transition duration-500 hover:scale-x-[1.2] overflow-hidden"></div>
                             <div className='absolute z-[30] inset-0 flex items-center justify-center p-4 pointer-events-none w-3/6'>
                                 <p className='text-white'>Explore Various options to Collaborating with teammates</p>
 
@@ -62,7 +62,7 @@ function Homepage() {
 
                 </div>
                 <div className='p-10 flex flex-col items-center justify-center w-3/4'>
-                    <Chart />
+                    
                 </div>
             </div>
         </>
--- src/Homepage/Stacks.jsx
+++ src/Homepage/Stacks.jsx
@@ -5,12 +5,15 @@ function Stacks() {
         <div className='relative flex items-start justify-self-start w-96 h-96 '>
                                     <div className='absolute opacity-75 bg-blue-600 z-10 inset-0 -translate-x-72 translate-y-12 transform rotate-45 rounded-lg' ></div>
                                     <div className='absolute opacity-60 bg-blue-600 z-20 inset-0 -translate-x-64 -translate-y-6 transform rotate-45 rounded-lg' ></div>
-                                    <div className='absolute opacity-65 bg-blue-600 z-[22] inset-0 -translate-x-40 translate-y-12 transform rotate-45 rounded-lg' ></div>
+                                    <div className='absolute opacity-50 bg-blue-600 z-[22] inset-0 -translate-x-40 translate-y-12 transform rotate-45 rounded-lg' ></div>
                                     <div className='absolute opacity-50 bg-blue-600 z-[23] inset-0 translate-x-30 translate-y-12 transform rotate-45 rounded-lg'  ></div>
                                     <div className='absolute opacity-50 bg-blue-600 z-[24] inset-0 translate-x-50 -translate-y-6 transform rotate-45 rounded-lg'  ></div>
                                     <div className='absolute opacity-50 bg-blue-600 z-[25] inset-0 translate-x-24 translate-y-12 transform rotate-45 rounded-lg' ></div>
                                     <div className='absolute opacity-50 bg-blue-600 z-[26] inset-0 translate-x-36 -translate-y-6 transform rotate-45 rounded-lg' ></div>
-                                    <div className='absolute opacity-85 bg-blue-600 z-30 inset-0 transform rotate-45 rounded-lg translate-x-64' ></div>
+                                    <div className='absolute opacity-50 bg-blue-600 z-[24] inset-0 -translate-x-24 -translate-y-6 transform rotate-45 rounded-lg'  ></div>
+                                    <div className='absolute opacity-50 bg-blue-600 z-[24] inset-0 translate-x-64 -translate-y-6 transform rotate-45 rounded-lg'  ></div>
+                                    <div className='absolute opacity-50 bg-blue-600 z-[24] inset-0 translate-x-60 translate-y-12 transform rotate-45 rounded-lg'  ></div>
+                                    <div className='absolute opacity-85 bg-blue-600 z-30 inset-0 transform rotate-45 rounded-lg translate-x-72' ></div>
                                 </div>
         </>
     )
