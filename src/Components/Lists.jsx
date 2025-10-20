c384d7ed5fbe8acdceb1d074acc461b7c8c6c165
kidist-hayalu <hkidist18hayalu@gmail.com>
2025-10-20 22:28:46 +0300

    Teams and NavBar

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -1,6 +1,6 @@
 import { BarChart3Icon, Settings, User } from "lucide-react";
 import { useState } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Lists() {
 
@@ -8,20 +8,20 @@ function Lists() {
         { id: 1, name: 'Projects', Link: '/Dashboard' },
         { id: 2, name: 'Teams', Link: '/Teams' },
         { id: 3, name: 'Reports', Link: '/Report' },
-        { id: 4, name: 'Chats', Link: '/Chats' },
-        { id: 5, name: 'Settings', Link: '/Settings' }];
+        { id: 4, name: 'Chats', Link: '/Chats' }];
 
     const navBarItems = navBar.map((bar) => <li key={bar.id} className="navBarItems hover:scale-110"><Link to={bar.Link}>{bar.name}</Link></li>);
 
     const [notification, setNotification] = useState(false);
     const [profile, setProfile] = useState(false);
+    const [settings, setSettings] = useState(false);
 
     return (<>
-        <header className='fixed top-0 left-0 w-full z-50 bg-white shadow-md bg-transparent'>
+        <header className='fixed top-0 left-0 w-full z-50 bg-white shadow-md bg-transparent '>
             <div className="sticky top-0">
                 <nav className="nav py-2">
-                    <div className="mr-96 flex justify-normal">
-                        <div className="flex-1 justify-self-start bg-gradient-to-tr from-cyan-500 via-cyan-500 to-cyan-600 rounded">
+                    <div className="mr-96 flex justify-normal ml-6">
+                        <div className="flex-1 justify-self-start mr-2 bg-gradient-to-tr from-cyan-500 via-cyan-500 to-cyan-600 rounded">
                             <BarChart3Icon style={{ color: "white", width: `1.65rem`, height: `1.65rem` }} />
                         </div>
                         <div className="flex-1 text-2xl font-semibold font-heading ">
@@ -31,6 +31,7 @@ function Lists() {
                     </div>
                     <ul className="list-none flex justify-center items-center gap-8 mr-80">{navBarItems}</ul>
                     <div className="flex flex-row items-center">
+                        <div className="flex flex-row items-center mr-3">
                         <div className="bg-cyan-500 rounded-full p-1 ">
                             <User size={18} />
                         </div>
@@ -40,17 +41,27 @@ function Lists() {
                             <p className="font-bold mb-2">User Name</p>
                             <p className="text-sm text-gray-600 mb-4">User Email</p>
                         </div>}
-                    </div>
-                    <div className="flex">
-                        <img src="/assets/bell-solid-full.svg" alt="Notifications Icon" className="relative w-6 h-6 mr-4 hover:cursor-pointer hover:scale-110" onClick={() => setNotification(!notification)}/>
-                        {notification && 
+                        </div>
+
+                        <div>
+                            <img src="/assets/gear-solid-full.svg" alt="Settings Icon" className="relative w-6 h-6 mr-4 hover:cursor-pointer hover:scale-110" onClick={() => setSettings(!settings)} />
+                        </div>
+                        {settings &&
+                            <div className="absolute top-12 right-12 w-48 bg-white border border-gray-300 rounded shadow-lg p-4">
+                                <p>Settings Menu</p></div>}
+                        <div>
+                            <img src="/assets/bell-solid-full.svg" alt="Notifications Icon" className="relative w-6 h-6 mr-4 hover:cursor-pointer hover:scale-110" onClick={() => setNotification(!notification)} />
+                        </div>
+                        {notification &&
                             <div className="absolute top-12 right-0 w-64 bg-white border border-gray-300 rounded shadow-lg p-4">
                                 <p>Notifications</p>
-                                </div>}
+                            </div>}
+
                     </div>
 
                 </nav>
-            </div></header>
+            </div>
+        </header>
     </>
     )
 }
--- src/Teams/Teams.jsx
+++ src/Teams/Teams.jsx
@@ -2,20 +2,27 @@ import { useState, useEffect } from 'react';
 import Lists from '../Components/Lists'
 import SearchBox from '../Components/SearchBox';
 import Data from '../assets/Data.json'
-import Card from '../Components/Card.jsx'
 
 function Teams() {
 
+    const [filter, setFilter] = useState("");
     const [isOpen, setIsOpen] = useState(null);
-    const data = Data.map((dat) => dat.TeamMembers);
-    const memberProgress = data.map((mp) => mp.progress);
-    
-    useEffect(() => {
+    const [filterOptions, setFilterOptions] = useState("all");
 
-        <div>{Data.ProjectTitle}</div>
-        
-    }
-        ,[isOpen])
+    const data = Data.map((dat) => dat.TeamMembers);
+    const type = Data.map((ti) => ti.ProjectType);
+
+    const filteredTypes = Data.filter(project => {
+        if (filterOptions === "all")
+            return true;
+        if (filterOptions === "individual")
+            return project.TeamMembers.length === 1;
+        if (filterOptions === "team")
+            return project.TeamMembers.length > 1;
+
+        return true;
+    });
+
 
     return (
         <>
@@ -25,20 +32,39 @@ function Teams() {
 
                     <SearchBox className='flex items-center justify-self-center ml-40' />
                 </div>
-                <div className='flex flex-col items-center justify-center w-full mx-12 mt-8 p-6 rounded shadow-md'>
-                    <div className='font-bold font-heading text-3xl flex items-start justify-start w-full'>
-                        <h2 >All Teams</h2>
+                <div className='flex flex-col items-center justify-center w-full mx-12 mt-8 p-6 '>
+                    <div className='flex flex-row items-center justify-center w-full'>
+                        <h2 className='font-bold font-heading text-3xl w-1/3 flex items-start justify-self-start'>{filterOptions.charAt(0).toUpperCase() + filterOptions.slice(1)} Teams</h2>
+                        <div className='mx-4 w-1/4'>
+                            <select id='filterOptions' value={filterOptions} onChange={e => setFilterOptions(e.target.value)} className='border rounded font-heading flex items-center justify-center w-full'>
+                                <option className='font-heading' value="all">All Projects</option>
+                                <option className='font-heading' value="individual">Individual Projects</option>
+                                <option className='font-heading' value="team">Team Projects</option>
+                            </select></div>
                     </div>
                     <div className='mx-12 w-full flex items-start justify-start'>
                         <div className='w-full'>
-                            {Data.map((title, index) => (<div className='border flex flex-row' key={index}>
-                                <li className='w-1/2'>{title.ProjectTitle}</li>
-                                <button className='flex items-center justify-self-center' onClick={() => {setIsOpen(isOpen === index ? null : index);}}>show members</button>
-                                {isOpen === index &&
-                                <div className='flex items-start justify-start flex-col'>
-                                   <ul>{title.TeamMembers.map((n, nIndex) => (<li className='list-none' key={nIndex}>{n.name}</li>))}</ul>
+                            {filteredTypes.map((title, index) => (<div className='border flex flex-col p-2 list-none my-2 w-4/5' key={index}>
+                                <div className='flex flex-row items-center justify-between'>
+                                    <li className=' font-heading font-semibold text-lg'>{title.ProjectTitle}</li>
+                                    <button className='text-sans text-sm transition-all duration-700 ease-in-out' onClick={() => { setIsOpen(isOpen === index ? null : index); }}>Members</button>
+                                </div>
+                                {isOpen === index && (
+                                    <div className='flex items-start justify-start flex-col overflow-hidden origin-top transition-all duration-700 ease-in-out w-full'>
+                                        <ul className='w-full'>{title.TeamMembers.map((n, nIndex) => (<div className='w-full flex'>
+                                            <li className='list-none mx-12 my-2 w-full' key={nIndex}>
+                                                <h3 className='text-lg font-semibold'>{n.name}</h3>
+                                                <p className='opacity-85 text-sm'>{n.activity}</p>
+                                                <div className='outerbox border-2 my-2 box-border rounded-xl overflow-hidden border-block-500 w-3/4'>
+                                                    <div className='innerbox text-right text-white h-full bg-cyan-600' style={{ width: `${n.progress}%`, transition: `width 0.5s ease` }}>{n.progress}%</div>
+                                                </div>
+                                                
+
+                                                </li>
+                                            </div>
+                                            ))}</ul>
                                     </div>
-                                }
+                                )}
                             </div>
                             ))}
                         </div>
